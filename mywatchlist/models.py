b82f971d48d0d4f2cbcafff7020578438bc7952f
Alia Widyanita Puspaningrum <alia.widyanita@ui.ac.id>
2022-11-24 23:09:21 +0700

    Add things

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class MyWatchList(models.Model):
-    watched = models.CharField(max_length=50)
+    watched = models.BooleanField()
     title = models.CharField(max_length=50)
-    rating = models.FloatField()
+    rating = models.CharField(max_length=50)
     release_date = models.TextField()
     review = models.TextField()
